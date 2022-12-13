5a19feaea67cfb13a8e0f81133d9d98c6a7b5bc2
Fleire Castro <fleire@thirdteam.org>
2022-12-14 04:21:21 +0800

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,58 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+//const basePromptPrefix = "generate a title of new korean love drama series for Netflix, setting: South Korea, and then write a detailed synopsis about this series where the protagonist name is:";
+const basePromptPrefix = "generate a list of content ideas for a business. list a minimum of 30 content ideas in bullet list.";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+
+/*  const secondPrompt = 
+  `
+  Take the character name and synopsis below and generate a script created in the style of Kim Won-seok. Make it feel like a movie. Don't just list the basic script. Include the description of the setting, a meet cute, and add conversations in the script:
+
+  Character Name: ${req.body.userInput}
+
+  Synopsis: ${basePromptOutput.text}
+
+  Script:
+  `
+*/
+  const secondPrompt = 
+  `
+  Refine the content ideas and include brand name and industry below. Make it unique. Then create a table with following columns - number, content title, media direction. Make sure the list is numbered and column 1 has a number. For the content title, write a catchy title in the style of Apple. For the media direction, include detailed instruction on what graphics or video to create based on the content idea. 
+
+  Brand Name and Industry: ${req.body.userInput}
+
+  Content Ideas: ${basePromptOutput.text}
+  `
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+    // I also increase max_tokens.
+    max_tokens: 1250,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,91 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import dashoContentLogo from '../assets/DOC - square logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+};
+const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <Image src={dashoContentLogo} alt="dashocontent logo" />
+            <h1>Dash of Content Ideas</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Stumped and out of content? Write your brand name and offer below and let's generate a list of content ideas:</h2>
           </div>
+            <br/><br/>
+            Example: DashoContent, a content subscription platform
+        </div>
+        {}
+        <div className="prompt-container">
+                  <textarea
+          className="prompt-box"
+          placeholder="start typing here"
+          value={userInput}
+          onChange={onUserChangedText}
+        />;
+      <div className="prompt-buttons">
+      <a className="generate-button" onClick={callGenerateEndpoint}>
+        <div className="generate">
+          {isGenerating ? <span class="loader"></span> : <p>Generate Ideas</p>}
+        </div>
+      </a>
+        </div>
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Here are some Content Ideas:</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
+
+
+
         </div>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="https://dashocontent.com/"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <Image src={dashoContentLogo} alt="dashocontent logo" />
+            <p>create with dashocontent</p>
           </div>
         </a>
       </div>
