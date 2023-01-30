95d86692c741bbd5bcc97b978319fdebefe3e999
Teslageek <dcastro@thirdteam.org>
2023-01-30 15:31:09 +0800

    epic

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,10 @@ export default function Document() {
     <Html>
       <Head>
         <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:description" content="The Debate Generator" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content=""
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
         
