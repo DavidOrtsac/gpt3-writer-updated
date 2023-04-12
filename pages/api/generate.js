a9af0e8c374cc606f49387fe54a5555eb95a66b8
DavidOrtsac <43810738+DavidOrtsac@users.noreply.github.com>
2023-04-13 00:09:56 +0800

    save point

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,7 +5,7 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-//const basePromptPrefix = "generate a title of new korean love drama series for Netflix, setting: South Korea, and then write a detailed synopsis about this series where the protagonist name is:";
+
 const basePromptPrefix = `You are a detailed and powerful, intelligent English teacher for stories and essays. Your goal is to analyze and then make worksheet questions and solution keys for stories and essays.
 Essay or Story. Analyze the story or essay. Then, create 4 difficult multiple-choice questions and solutions based on the essay and 2 true/false questionnaires with answers, and 1 essay question portion:\n`;
 const generateAction = async (req, res) => {
@@ -33,7 +33,7 @@ HTML and CSS code:\n
 */
 const secondPrompt = 
   `
-  Then, place the entire essay or story and its questions, choices, and answers in an HTML file with a modern CSS design inside style tags. The text must always be black. The background must always be white. The essay or story must be in a box. Each question and choices must be inside their own curved boxes. DO NOT put shadows. The answer must be in a curved box. The whole document must be inside a black-bordered curved box.
+  Then, place the entire essay or story and its questions, choices, and answers in an HTML file with a modern, clean, beautiful, and simple CSS design inside style tags. The text must always be black. Do not style the background. The essay or story must be in a box. Each question and choices must be inside their own curved boxes. DO NOT put shadows. The answer must be in a colored, curved box. The whole document must be inside a black thin-bordered curved box.
 Essay/Story:\n${req.body.userInput}
 Choices and Answers:\n${basePromptOutput.text}
 HTML and CSS code:\n
@@ -41,13 +41,11 @@ HTML and CSS code:\n
   const secondPromptCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${secondPrompt}`,
-    // I set a higher temperature for this one. Up to you!
-    temperature: 0.85,
-    // I also increase max_tokens.
+    temperature: 0.8,
     max_tokens: 2550,
   });
   
-  // Get the output
+  // Grab output
   const secondPromptOutput = secondPromptCompletion.data.choices.pop();
 
   res.status(200).json({ output: secondPromptOutput });
